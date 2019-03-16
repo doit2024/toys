3b335456e31107d99ec9aa5879b5f14e9d5456ab
邓维敏 <WeiminDeng@sf-express.com>
2019-03-16 15:43:20 +0800

    1

--- dbal.js
+++ dbal.js
@@ -8,7 +8,7 @@ superagent
   .end(function(err,pres){
     const $ = cheerio.load(pres.text);
     const boxes = $('.ballbox');
-    const data = boxes.map((v, i) => boxes.eq(i).text()).map(v => v.split(/[^\d]+/).filter(v => v).map(v => v * 1))
+    const data = boxes.map((v, i) => boxes.eq(i).text())
     console.log(data)
     // for(let i = 0 ; i < boxes.length ; i++){
     //   let nums = boxes.eq(i).text();
