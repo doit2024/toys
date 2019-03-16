fe328790574b57306ac57d8a15ba0379cfe54d7e
邓维敏 <WeiminDeng@sf-express.com>
2019-03-16 15:11:13 +0800

    ..

--- dbal.js
+++ dbal.js
@@ -1,10 +1,16 @@
 const superagent = require("superagent")
+const cheerio = require("cheerio")
 
 const pageUrl = `https://kjh.55128.cn/ssq-history-120.htm`
 
 superagent
   .get(pageUrl)
   .end(function(err,pres){
-    console.log(err, pres)
+    var $ = cheerio.load(pres.text);
+    var boxes = $('.ballbox');
+
+    for(var i = 0 ; i < boxes.length ; i++){
+      var nums = boxes.eq(i).text();
+      console.log(nums)
+    }
   });
-  
