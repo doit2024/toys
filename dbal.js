b2a0b25609abe30898c9adae3212b687ecb7f9f2
邓维敏 <WeiminDeng@sf-express.com>
2019-03-16 16:14:47 +0800

    1

--- dbal.js
+++ dbal.js
@@ -1,6 +1,7 @@
 const superagent = require("superagent")
 const cheerio = require("cheerio")
 
+// 最新 120 条数据
 const pageUrl = `https://kjh.55128.cn/ssq-history-120.htm`
 
 superagent
@@ -8,7 +9,14 @@ superagent
   .end(function(err,pres){
     const $ = cheerio.load(pres.text);
     const boxes = $('.ballbox');
-    const data = [].slice.call(boxes).map((v, i) => boxes.eq(i).text()).map(v => v.split(/[^\d]+/).filter(v => v).map(v => v * 1))
+    const data = [].slice.call(boxes)
+      .map((v, i) => boxes.eq(i).text())
+      .map(v => v.split(/[^\d]+/).filter(v => v)
+      .map(v => v * 1))
     const rst = data.reduce((p, xs) => (xs.forEach((x, i) => p[i] ? (p[i].push(x)) : (p[i] = [])), p), [])
-    console.log(rst)
+    // 统计出现次数
+    const counts = rst
+      .map(xs => xs.sort((a, b) => (a - b)))
+      .reduce((p, xs) => { xs.forEach(x => p[x] ? (p[x]++) : (p[x] = 0) ) }, {})
+    console.log(counts)
   });
