8799e24aed7ff8a6016888038a7d89f3f704a66b
邓维敏 <WeiminDeng@sf-express.com>
2019-03-16 15:42:40 +0800

    1

--- dbal.js
+++ dbal.js
@@ -6,11 +6,12 @@ const pageUrl = `https://kjh.55128.cn/ssq-history-120.htm`
 superagent
   .get(pageUrl)
   .end(function(err,pres){
-    var $ = cheerio.load(pres.text);
-    var boxes = $('.ballbox');
-
-    for(var i = 0 ; i < boxes.length ; i++){
-      var nums = boxes.eq(i).text();
-      console.log(nums.split(/[^\d]+/).filter(v => v).map(v => v * 1))
-    }
+    const $ = cheerio.load(pres.text);
+    const boxes = $('.ballbox');
+    const data = boxes.map((v, i) => boxes.eq(i).text()).map(v => v.split(/[^\d]+/).filter(v => v).map(v => v * 1))
+    console.log(data)
+    // for(let i = 0 ; i < boxes.length ; i++){
+    //   let nums = boxes.eq(i).text();
+    //   console.log(nums.split(/[^\d]+/).filter(v => v).map(v => v * 1))
+    // }
   });
